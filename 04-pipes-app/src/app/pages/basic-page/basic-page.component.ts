966a29da333ba5746749374bba80a18d092bcb9c
Leidi <leididelapuente3@gmail.com>
2025-06-05 17:52:29 -0500

    (pipes app): date pipes

--- 04-pipes-app/src/app/pages/basic-page/basic-page.component.ts
+++ 04-pipes-app/src/app/pages/basic-page/basic-page.component.ts
@@ -1,13 +1,16 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
 })
 export default class BasicPageComponent {
   nameLower = signal('aaron');
   nameUpper = signal('AARON');
   fullName = signal('aAroN wArNeR');
+
+  customDate = signal(new Date());
+
 }
