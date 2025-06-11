2cfef59085baf52e8abe1410d7bb950e3492fefd
Leidi De La Puente <leididelapuente3@gmail.com>
2025-06-11 10:27:08 -0500

    (pipes app): add notes

--- 04-pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ 04-pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -7,14 +7,22 @@ import { ColorPipe } from '../../pipes/color.pipe';
 import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
 import { TitleCasePipe } from '@angular/common';
 import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
+import { HeroSortByPipe } from '../../pipes/hero-sort-by.pipe';
+import { HeroFilterPipe } from '../../pipes/hero-filter.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe, CanFlyPipe, ColorPipe, HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe],
+  imports: [ToggleCasePipe, CanFlyPipe, ColorPipe, HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe, HeroSortByPipe, HeroFilterPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
   name = signal('Aaron');
   turnUpper = signal<boolean>(true);
   heroes = signal<Hero[]>(heroes);
+  sortBy = signal<keyof Hero | null>(null);
+  searchQuery = signal<string>('');
+
+  updateSortBy(value: keyof Hero){
+    this.sortBy.set(value);
+  }
 }
